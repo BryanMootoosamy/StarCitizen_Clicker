02a317be659b5b7b3db320579a84abc7f2fd1ccf
Bryan Mootoosamy <mootoosamy.bryan@gmail.com>
2018-07-17 10:51:45 +0200

    first version of clicker

--- App.js
+++ App.js
@@ -4,47 +4,41 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  View,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  StatusBar
-} from 'react-native';
+import React from "react";
+import {View, StyleSheet, ImageBackground, Dimensions, StatusBar} from "react-native";
 import background from "./assets/images/background.jpg";
-import Clicker from "./Components/clicker.js"
+import Clicker from "./Components/clicker.js";
 
 
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    let {width, height} = Dimensions.get("window");
-    this.state = {
-      width: width,
-      height: height
+    constructor(props) {
+        super(props);
+        let {width, height} = Dimensions.get("window");
+        this.state = {
+            width: width,
+            height: height
+        };
     }
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <View style={{width: this.state.width}}>
-         <ImageBackground source={background} style={{width: this.state.width, height: this.state.height}}>
-          <Clicker />
-         </ImageBackground>
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true} />
+                <View style={{width: this.state.width}}>
+                    <ImageBackground source={background} style={{width: this.state.width, height: this.state.height}}>
+                        <Clicker screenWidth={this.state.width} screenHeight={this.state.height} />
+                    </ImageBackground>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+    },
 });
--- Components/clicker.js
+++ Components/clicker.js
@@ -1,12 +1,29 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, View, Image, TouchableOpacity} from "react-native";
+import ship from "../assets/images/ship.png";
 
 export default class Clicker extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            score: 0,
+            height: this.props.screenHeight,
+            width: this.props.screenWidth
+        };
+    }
+    clicker = () => {
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
     render(){
         return(
             <View>
-               <Text style={{color:"#FFF"}}>Lol</Text>
+                <TouchableOpacity activeOpacity={1} onPress={this.clicker}>
+                    <Image source={ship} style={{width: this.state.width / 1.1, height: this.state.height / 1.1, resizeMode: "contain"}} />
+                </TouchableOpacity>
+                <Text style={{color: "#fff"}}>{this.state.score}</Text>
             </View>
-        )
+        );
     }
 }
